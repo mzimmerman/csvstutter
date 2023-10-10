1b1802bbe7ef6f0238e59b42a9b5191b3da3b9eb
mzimmerman <2413139+mzimmerman@users.noreply.github.com>
2023-10-10 14:34:55 +0000

    unstutter data in one channel and write that data to a buffer (and have it read) in the Read() call by the client

--- csvstutter.go
+++ csvstutter.go
@@ -5,34 +5,32 @@ import (
 	"encoding/csv"
 	"io"
 	"strings"
-	"sync"
 )
 
 type Reader struct {
-	err      error
 	reader   *csv.Reader
-	done     chan struct{}
-	lock     sync.Mutex
-	toBeRead chan *bytes.Buffer
+	done     chan error
+	toBeRead *bytes.Buffer
+	toWrite  chan []string
+	writer   *csv.Writer
 }
 
 func NewReader(rdr io.Reader) *Reader {
 	reader := csv.NewReader(rdr)
 	reader.LazyQuotes = true
 	reader.FieldsPerRecord = -1 // disabling this check
-	toUnstutter := make(chan []string, 100)
-	toWrite := make(chan []string, 100)
-
+	buf := &bytes.Buffer{}
 	r := &Reader{
 		reader:   reader,
-		done:     make(chan struct{}),
-		toBeRead: make(chan *bytes.Buffer),
+		done:     make(chan error),
+		toBeRead: buf,
+		toWrite:  make(chan []string, 100),
+		writer:   csv.NewWriter(buf),
 	}
-	toWriteCopy := toWrite // create local variable copy for 3rd goroutine later
 	go func() {
-		// read lines from rdr
+		// read and unstutter them!
 		defer func() {
-			close(toUnstutter)
+			close(r.toWrite)
 		}()
 		for {
 			line, err := reader.Read()
@@ -40,24 +38,15 @@ func NewReader(rdr io.Reader) *Reader {
 				return
 			}
 			if err != nil {
-				r.lock.Lock()
-				r.err = err
-				r.lock.Unlock()
+				select {
+				case _, ok := <-r.done:
+					_ = ok // we don't use this but I need to receive a value for the compiler
+					// done processing
+				case r.done <- err:
+					// error was sent, stop reading
+				}
 				return
 			}
-			select {
-			case <-r.done:
-				return
-			case toUnstutter <- line:
-			}
-		}
-	}()
-	go func() {
-		// unstutter them!
-		defer func() {
-			close(toWrite)
-		}()
-		for line := range toUnstutter {
 			for x := range line {
 				idx := strings.Index(line[x], "\n")
 				if idx == -1 {
@@ -74,41 +63,11 @@ func NewReader(rdr io.Reader) *Reader {
 				line[x] = line[x][:leftIdx]
 			}
 			select {
-			case <-r.done:
-				return
-			case toWrite <- line:
+			case _, ok := <-r.done:
+				_ = ok // we don't use this but I need to receive a value for the compiler
+				// done processing
+			case r.toWrite <- line:
 				// log.Printf("sent line toWrite")
-			}
-		}
-	}()
-	go func() {
-		// write it!
-		buf := &bytes.Buffer{}
-		csvwriter := csv.NewWriter(buf)
-		doneReading := false
-		allowReading := (chan *bytes.Buffer)(nil)
-		for {
-			select {
-			case line, ok := <-toWriteCopy:
-				if !ok { // toWrite is closed
-					doneReading = true
-					toWriteCopy = nil
-					// log.Printf("toWrite is closed")
-					continue
-				}
-				csvwriter.Write(line)
-				csvwriter.Flush()
-				allowReading = r.toBeRead // only now should buf be sent to read()
-				// log.Printf("allowReading set to r.toBeRead")
-			case allowReading <- buf:
-				// log.Printf("sent buf to read()")
-				<-allowReading // get it back from the read function
-				// log.Printf("received buf from read()")
-			case <-r.done:
-			}
-			if doneReading && buf.Len() == 0 {
-				close(allowReading)
-				return
 			}
 		}
 	}()
@@ -123,24 +82,17 @@ func (reader *Reader) Close() {
 // Read returns only valid CSV data as read from the source and removes multilines and stutter
 // if there's an error all subsequent calls to Read will fail with the same error
 func (reader *Reader) Read(lineIn []byte) (int, error) {
-	reader.lock.Lock()
-	err := reader.err
-	reader.lock.Unlock()
-	if err != nil {
-		return 0, err
+	if reader.toBeRead.Len() == 0 {
+		select {
+		case line, ok := <-reader.toWrite:
+			if !ok {
+				return 0, io.EOF
+			}
+			reader.writer.Write(line)
+			reader.writer.Flush()
+		case err := <-reader.done:
+			return 0, err
+		}
 	}
-	// log.Printf("read() called")
-	buf, ok := <-reader.toBeRead
-	// log.Printf("read got the buffer")
-	if !ok {
-		// log.Printf("EOF in Read() call")
-		return 0, io.EOF
-	}
-	num, err := buf.Read(lineIn)
-	reader.toBeRead <- nil
-	if num == 0 && err == io.EOF {
-		return reader.Read(lineIn)
-	}
-	// log.Printf("read() returned %d for %v", num, err)
-	return num, err
+	return reader.toBeRead.Read(lineIn)
 }
