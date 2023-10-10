e3414953d90ce1084f3f849e56cf3e4d943608b5
mzimmerman <2413139+mzimmerman@users.noreply.github.com>
2023-10-10 13:14:57 +0000

    parallel work

--- csvstutter.go
+++ csvstutter.go
@@ -5,60 +5,142 @@ import (
 	"encoding/csv"
 	"io"
 	"strings"
+	"sync"
 )
 
 type Reader struct {
 	err      error
 	reader   *csv.Reader
-	toReturn *bytes.Buffer
-	writer   *csv.Writer
+	done     chan struct{}
+	lock     sync.Mutex
+	toBeRead chan *bytes.Buffer
 }
 
 func NewReader(rdr io.Reader) *Reader {
-	buffer := &bytes.Buffer{}
 	reader := csv.NewReader(rdr)
 	reader.LazyQuotes = true
 	reader.FieldsPerRecord = -1 // disabling this check
-	return &Reader{
+	toUnstutter := make(chan []string, 100)
+	toWrite := make(chan []string, 100)
+
+	r := &Reader{
 		reader:   reader,
-		writer:   csv.NewWriter(buffer),
-		toReturn: buffer,
+		done:     make(chan struct{}),
+		toBeRead: make(chan *bytes.Buffer),
 	}
+	toWriteCopy := toWrite // create local variable copy for 3rd goroutine later
+	go func() {
+		// read lines from rdr
+		defer func() {
+			close(toUnstutter)
+		}()
+		for {
+			line, err := reader.Read()
+			if err == io.EOF {
+				return
+			}
+			if err != nil {
+				r.lock.Lock()
+				r.err = err
+				r.lock.Unlock()
+				return
+			}
+			select {
+			case <-r.done:
+				return
+			case toUnstutter <- line:
+			}
+		}
+	}()
+	go func() {
+		// unstutter them!
+		defer func() {
+			close(toWrite)
+		}()
+		for line := range toUnstutter {
+			for x := range line {
+				idx := strings.Index(line[x], "\n")
+				if idx == -1 {
+					continue // no mulitple lines here
+				}
+				leftIdx := idx
+				if idx > 0 && line[x][idx-1] == '}' {
+					leftIdx--
+				}
+				if line[x][:leftIdx] != line[x][idx+1:] {
+					continue // not a stutter, not the same
+				}
+				// yes, we're a stutter value! remove it
+				line[x] = line[x][:leftIdx]
+			}
+			select {
+			case <-r.done:
+				return
+			case toWrite <- line:
+				// log.Printf("sent line toWrite")
+			}
+		}
+	}()
+	go func() {
+		// write it!
+		buf := &bytes.Buffer{}
+		csvwriter := csv.NewWriter(buf)
+		doneReading := false
+		allowReading := (chan *bytes.Buffer)(nil)
+		for {
+			select {
+			case line, ok := <-toWriteCopy:
+				if !ok { // toWrite is closed
+					doneReading = true
+					toWriteCopy = nil
+					// log.Printf("toWrite is closed")
+					continue
+				}
+				csvwriter.Write(line)
+				csvwriter.Flush()
+				allowReading = r.toBeRead // only now should buf be sent to read()
+				// log.Printf("allowReading set to r.toBeRead")
+			case allowReading <- buf:
+				// log.Printf("sent buf to read()")
+				<-allowReading // get it back from the read function
+				// log.Printf("received buf from read()")
+			case <-r.done:
+			}
+			if doneReading && buf.Len() == 0 {
+				close(allowReading)
+				return
+			}
+		}
+	}()
+	return r
+}
+
+// Close cleans up the resources created to read the file multicore style
+func (reader *Reader) Close() {
+	close(reader.done)
 }
 
 // Read returns only valid CSV data as read from the source and removes multilines and stutter
 // if there's an error all subsequent calls to Read will fail with the same error
-func (reader Reader) Read(lineIn []byte) (int, error) {
-	if reader.err != nil {
-		return 0, reader.err
+func (reader *Reader) Read(lineIn []byte) (int, error) {
+	reader.lock.Lock()
+	err := reader.err
+	reader.lock.Unlock()
+	if err != nil {
+		return 0, err
 	}
-	if reader.toReturn.Len() == 0 { // nothing to copy, need another line loaded
-		line, err := reader.reader.Read()
-		if err != nil {
-			reader.err = err
-			return 0, err
-		}
-		for x := range line {
-			idx := strings.Index(line[x], "\n")
-			if idx == -1 {
-				continue // no mulitple lines here
-			}
-			leftIdx := idx
-			if idx > 0 && line[x][idx-1] == '}' {
-				leftIdx--
-			}
-			if line[x][:leftIdx] != line[x][idx+1:] {
-				continue // not a stutter, not the same
-			}
-			// yes, we're a stutter value! remove it
-			line[x] = line[x][:leftIdx]
-		}
-		err = reader.writer.Write(line)
-		if err != nil {
-			reader.err = err
-			return 0, err
-		}
-		reader.writer.Flush()
+	// log.Printf("read() called")
+	buf, ok := <-reader.toBeRead
+	// log.Printf("read got the buffer")
+	if !ok {
+		// log.Printf("EOF in Read() call")
+		return 0, io.EOF
 	}
-	return reader.toReturn.Read(lineIn)
+	num, err := buf.Read(lineIn)
+	reader.toBeRead <- nil
+	if num == 0 && err == io.EOF {
+		return reader.Read(lineIn)
+	}
+	// log.Printf("read() returned %d for %v", num, err)
+	return num, err
 }
--- csvstutter_test.go
+++ csvstutter_test.go
@@ -1,7 +1,13 @@
 package csvstutter_test
 
 import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
 	"strings"
 	"testing"
 
@@ -27,5 +33,61 @@ csv,is,fun
 	}
 	if expected != string(got) {
 		t.Errorf("\nexpected: %q\ngot     : %q", expected, got)
+	}
+}
+
+var bigResults = [][]string{}
+
+func BenchmarkStutter(b *testing.B) {
+	dataIn, err := os.ReadFile("data.out")
+	if err != nil {
+		log.Fatalf("error reading in file for testing")
+	}
+	b.ResetTimer()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		stutter := csvstutter.NewReader(bytes.NewReader(dataIn))
+		results, err := csv.NewReader(stutter).ReadAll()
+		if err != nil {
+			b.Errorf("error reading csv - %v", err)
+		}
+		stutter.Close()
+		bigResults = results
+	}
+}
+
+// generateData makes a csvstutter file for testing
+func generateData() {
+	chars := `qwertyuiopasdfghjklzxcvbnm,"'1234567890`
+	f, err := os.OpenFile("data.out", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
+	if err != nil {
+		log.Fatalf("error opening file - %v", err)
+	}
+	defer f.Close()
+	var field bytes.Buffer
+	var line []string
+	var stutterLine []string
+	writer := csv.NewWriter(f)
+	defer writer.Flush()
+	for x := 0; x < 50000; x++ {
+		line = line[:0]
+		stutterLine = stutterLine[:0]
+		for y := 0; y < 25; y++ {
+			length := rand.Intn(200) + 25
+			field.Reset()
+			for z := 0; z < length; z++ {
+				field.WriteByte(chars[rand.Intn(len(chars))])
+			}
+			line = append(line, field.String())
+			stutterLine = append(stutterLine, fmt.Sprintf("%s\n%s", field.Bytes(), field.Bytes()))
+		}
+		err = writer.Write(line)
+		if err != nil {
+			log.Fatalf("error writing line - %v", err)
+		}
+		err = writer.Write(stutterLine)
+		if err != nil {
+			log.Fatalf("error writing stutterline - %v", err)
+		}
 	}
 }
